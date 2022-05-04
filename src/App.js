9b19f86bf29168548faea38dfe6c6fb1f33e4223
unknown <oladejofavourajoke@gmail.com>
2022-05-04 15:02:38 +0100

    textarea added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-       <h1>Hello</h1>
+    <div>
+      <h1>Convert your Markdown</h1>
+      <div className="row"></div>
+      <div className="col-6">
+        <textarea id='editor'/>
+      </div>
+      <div className="col-6"></div>
     </div>
   );
 }
+
 
 export default App;
